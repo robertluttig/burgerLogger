43e9e93d9d76e2625e2d65053dbdc87e13329d10
Robert Luttig <robert.luttig@gmail.com>
2020-07-17 19:29:17 -0700

    aded connection to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "Password",
   database: "burgers_db",
 });
-
+};
 // Make connection.
 connection.connect((err) => {
   if (err) {
