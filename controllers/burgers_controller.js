24c1f673cc6e406a0ea24d694bf136d46e1d199a
Robert Luttig <robert.luttig@gmail.com>
2020-07-18 16:57:09 -0700

    added  burger controls for post, put

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,4 +12,38 @@ router.get("/", (req, res) => {
     res.render("index", hbsObject);
   });
 });
+router.post("/api/burgers", (req, res) => {
+  burger.create({ burger_name: req.body.name, devoured: req.body.devoured }, (result) => {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+// using put to replace the value of sleepy for a
+// specific cat resource
+router.put("/api/burgers/:id/devoured", (req, res) => {
+  const condition = { id: req.params.id };
+  const update = { sdevoured: req.body.value };
+
+  burger.update(update, condition, (result) => {
+    if (result.affectedRows === 0) {
+      // If no rows were affected, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
+
+router.delete("/api/burgers/:id", (req, res) => {
+  const condition = { id: req.params.id };
+
+  burger.delete(condition, (result) => {
+    if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } 
+    res.status(200).end();
+  });
+});
+
 module.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
   
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+-- INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+-- INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+-- INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
